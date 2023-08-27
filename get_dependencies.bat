cc7ebd881953d0ee5b7b790727bd99dc9512d6da
Alin Iordache <iordache.alin@yahoo.com>
2023-08-27 10:56:41 +0300

    UI tweaks, mdx updates and get_dependencies script

---
+++ get_dependencies.bat
@@ -0,0 +1,10 @@
+:: Copy bundle.js and 838.bundle.js from the Algovis repository to the assets and dist folders
+copy /Y "..\Algo-Vis\build\838.bundle.js" "..\algo-vis-astro\public\assets\scripts\"
+copy /Y "..\Algo-Vis\build\bundle.js" "..\algo-vis-astro\public\assets\scripts\"
+
+copy /Y "..\Algo-Vis\build\838.bundle.js" "..\algo-vis-astro\dist\assets\scripts\"
+copy /Y "..\Algo-Vis\build\bundle.js" "..\algo-vis-astro\dist\assets\scripts\"
+
+xcopy /E /Y /F "..\Algo-Vis\build\wordpress\quizzes" "..\algo-vis-astro\public\assets\algovis\quizzes\"
+xcopy /E /Y /F "..\Algo-Vis\build\wordpress\snippets" "..\algo-vis-astro\public\assets\algovis\snippets\"
+xcopy /E /Y /F "..\Algo-Vis\build\wordpress\exercises" "..\algo-vis-astro\public\assets\algovis\exercises\"
