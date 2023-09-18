656fb9104f2122b11d6789cfb21d1bfdc5f770c0
Alin Iordache <iordache.alin@yahoo.com>
2023-09-18 21:34:31 +0300

    Updated worker thread name

--- get_dependencies.bat
+++ get_dependencies.bat
@@ -1,8 +1,8 @@
-:: Copy bundle.js and 838.bundle.js from the Algovis repository to the assets and dist folders
-copy /Y "..\Algo-Vis\build\838.bundle.js" "..\algo-vis-astro\public\assets\scripts\"
+:: Copy bundle.js and 310.bundle.js from the Algovis repository to the assets and dist folders
+copy /Y "..\Algo-Vis\build\310.bundle.js" "..\algo-vis-astro\public\assets\scripts\"
 copy /Y "..\Algo-Vis\build\bundle.js" "..\algo-vis-astro\public\assets\scripts\"
 
-copy /Y "..\Algo-Vis\build\838.bundle.js" "..\algo-vis-astro\dist\assets\scripts\"
+copy /Y "..\Algo-Vis\build\310.bundle.js" "..\algo-vis-astro\dist\assets\scripts\"
 copy /Y "..\Algo-Vis\build\bundle.js" "..\algo-vis-astro\dist\assets\scripts\"
 
 xcopy /E /Y /F "..\Algo-Vis\build\wordpress\quizzes" "..\algo-vis-astro\public\assets\algovis\quizzes\"
