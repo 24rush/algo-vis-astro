abaea6e43ecf0dea3603dcf4a391e11e58adfa86
Alin Iordache <iordache.alin@yahoo.com>
2023-10-02 09:39:18 +0300

    Redesigned exercises

--- get_dependencies.bat
+++ get_dependencies.bat
@@ -6,5 +6,3 @@ copy /Y "..\Algo-Vis\build\310.bundle.js" "..\algo-vis-astro\dist\assets\scripts
 copy /Y "..\Algo-Vis\build\bundle.js" "..\algo-vis-astro\dist\assets\scripts\"
 
 xcopy /E /Y /F "..\Algo-Vis\build\wordpress\quizzes" "..\algo-vis-astro\public\assets\algovis\quizzes\"
-xcopy /E /Y /F "..\Algo-Vis\build\wordpress\snippets" "..\algo-vis-astro\public\assets\algovis\snippets\"
-xcopy /E /Y /F "..\Algo-Vis\build\wordpress\exercises" "..\algo-vis-astro\public\assets\algovis\exercises\"
